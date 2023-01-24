af741597034cc7b492c3c366980270fbde61b96b
Elie <elie.welcom@gmail.com>
2023-01-24 01:14:06 +0100

    Version apure

--- courbe.cpp
+++ courbe.cpp
@@ -7,13 +7,17 @@
 Courbe::Courbe(int nb_point, Point_inter point_inter, Point point_control_1, Point point_control_2) {
     _nb_point = nb_point;
 
-    float mid_1_x, mid_1_y, mid_2_x, mid_2_y, mid_courbe_1, mid_courbe_2;
+    float mid_1_x, mid_1_y, mid_2_x, mid_2_y;
+    double t;
 
-    for (int i = 0; i == _nb_point + 1; i++){
-        _t = i / _nb_point;
+    for (int i = 0; i < _nb_point + 1; i++){
+        //On utilise une version float de nb_point pour eviter la division inttegrante
+        t = i / float(nb_point);
 
-        mid_1_x = (1 - _t) * point_control_1.GetX() + _t * point_inter.GetInterPointX();
-        mid_1_y = (1 - _t) * point_control_1.GetY() + _t * point_inter.GetInterPointY();
+        mid_1_x = (1 - t) * point_control_1.GetX() + t * point_inter.GetInterPointX();
+        mid_1_y = (1 - t) * point_control_1.GetY() + t * point_inter.GetInterPointY();
+
+        std::cout << std::to_string(mid_1_x) << " " << std::to_string(mid_1_y) << std::endl;
 
         mid_2_x = (1 - _t) * point_inter.GetInterPointX() + _t * point_control_2.GetX();
         mid_2_y = (1 - _t) * point_inter.GetInterPointY() + _t * point_control_2.GetY();
@@ -22,7 +26,7 @@ Courbe::Courbe(int nb_point, Point_inter point_inter, Point point_control_1, Poi
         _list_point_segment_2.emplace_back(mid_2_x, mid_2_y);
 
         _list_point_courbe.emplace_back(_t * mid_2_x + (1 - _t) * mid_1_x, _t * mid_2_y + (1 - _t) * mid_1_y);
-        std::cout << std::to_string(_list_point_courbe[i].GetX()) << " " << std::to_string(_list_point_courbe[i].GetY()) << std::endl;
+        //std::cout << std::to_string(i) << " " << std::to_string(_list_point_courbe[i].GetX()) << " " << std::to_string(_list_point_courbe[i].GetY()) << std::endl;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -3,19 +3,7 @@
 #include "courbe.h"
 
 
-//Importation de OpenGL
-#include "OpenGL/glm/glm/vec3.hpp" // glm::vec3
-#include "OpenGL/glm/glm/vec4.hpp" // glm::vec4
-#include "OpenGL/glm/glm/mat4x4.hpp" // glm::mat4
-#include "OpenGL/glm/glm/ext/matrix_transform.hpp" // glm::translate, glm::rotate, glm::scale
-#include "OpenGL/glm/glm/ext/matrix_clip_space.hpp" // glm::perspective
-#include "OpenGL/glm/glm/ext/scalar_constants.hpp" // glm::pi
 
-
-//#include "OpenGL/GLFW/from_github/glfw/include/GLFW/glfw3.h"
-
-#include "windows.h"
-#include "GL/glu.h"
 
 int main(){
     //Definition des points qui portent les 2 segments
